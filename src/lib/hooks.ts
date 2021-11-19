c9f2b7b91e0b7eff65f046c57ef2b0d765019ea4
Alan R. Soares <alanrsoars@gmail.com>
2021-11-19 17:40:55 +1300

    chore: implement walletconnect provider

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -4,10 +4,23 @@ import { useMutation } from "react-query";
 import { Web3Provider, ExternalProvider } from "@ethersproject/providers";
 import { Contract } from "@ethersproject/contracts";
 
-import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
-const providerOptions = {
-  /* See Provider Options Section */
+import Web3Modal from "web3modal";
+import detectEthereumProvider from "@metamask/detect-provider";
+
+const INFURA_ID = "9568a787fc7f46739d810321a2e92955";
+
+const PROVIDER_OPTIONS = {
+  injected: {
+    package: "",
+  },
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: INFURA_ID, // required
+    },
+  },
 };
 
 export function useWeb3Provider() {
@@ -18,16 +31,37 @@ export function useWeb3Provider() {
   useEffect(() => {
     async function task() {
       try {
-        const web3Modal = new Web3Modal({
-          network: "mainnet", // optional
-          cacheProvider: true, // optional
-          providerOptions, // required
-        });
-
         setIsLoading(true);
-        const externalProvider = await web3Modal.connect();
+        const externalProvider = await detectEthereumProvider();
+
         if (externalProvider) {
-          setProvider(new Web3Provider(externalProvider as ExternalProvider));
+          const provider = new Web3Provider(
+            externalProvider as ExternalProvider
+          );
+
+          setProvider(provider);
+
+          provider.on("accountsChanged", (accounts: string[]) => {
+            console.log("accountsChanged", accounts);
+          });
+
+          // Subscribe to chainId change
+          provider.on("chainChanged", (chainId: number) => {
+            console.log("chainChanged", chainId);
+          });
+
+          // Subscribe to provider connection
+          provider.on("connect", (info: { chainId: number }) => {
+            console.log("connect", info);
+          });
+
+          // Subscribe to provider disconnection
+          provider.on(
+            "disconnect",
+            (error: { code: number; message: string }) => {
+              console.log("disconnect", error);
+            }
+          );
         }
       } catch (error) {
         setError(error);
@@ -41,6 +75,25 @@ export function useWeb3Provider() {
     }
   }, [provider]);
 
+  const handleTryConnect = useCallback(async () => {
+    try {
+      const web3Modal = new Web3Modal({
+        cacheProvider: true,
+        providerOptions: PROVIDER_OPTIONS,
+      });
+      const externalProvider = await web3Modal.connect();
+
+      if (externalProvider) {
+        const provider = new Web3Provider(externalProvider as ExternalProvider);
+        setProvider(provider);
+        return provider;
+      }
+    } catch (error) {
+      console.log("Failed to connect to web3");
+      return null;
+    }
+  }, []);
+
   const isError = useMemo(() => Boolean(error), [error]);
 
   return {
@@ -48,6 +101,7 @@ export function useWeb3Provider() {
     error,
     isError,
     isLoading,
+    tryConnect: handleTryConnect,
   };
 }
 
@@ -60,12 +114,13 @@ export type Web3User =
 
 export function useWeb3Auth() {
   const [user, setUser] = useState<Web3User>({ kind: "anonymous" });
-  const { provider } = useWeb3Provider();
+  const { provider, tryConnect } = useWeb3Provider();
 
   const resumeSession = useCallback(async () => {
     try {
       if (!provider) {
-        console.log("no provider");
+        console.log("no provider detected");
+
         return false;
       }
 
@@ -85,22 +140,25 @@ export function useWeb3Auth() {
   const signinMutation = useCallback(async () => {
     const resumed = await resumeSession();
 
-    if (resumed || !provider) {
+    if (resumed) {
       return;
     }
 
     try {
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
-      const address = await signer.getAddress();
-      setUser({
-        kind: "connected",
-        address,
-      });
+      const $provider = provider || (await tryConnect());
+      if ($provider) {
+        await $provider.send("eth_requestAccounts", []);
+        const signer = $provider.getSigner();
+        const address = await signer.getAddress();
+        setUser({
+          kind: "connected",
+          address,
+        });
+      }
     } catch (error) {
       console.log("failed to connect", error);
     }
-  }, [provider, resumeSession]);
+  }, [provider, resumeSession, tryConnect]);
 
   const { mutateAsync: signin, isLoading: isSigningIn } =
     useMutation(signinMutation);
