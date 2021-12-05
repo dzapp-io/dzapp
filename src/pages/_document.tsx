3a79a2d4f6231e53ecb7de27ee391a2e79bbb20f
Alan R. Soares <alanrsoars@gmail.com>
2021-12-05 22:40:47 +1300

    chore: add next-pwa support

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,9 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="theme-color" content="rgb(17, 24, 39)" />
           <meta name="description" content="Web3 automation made simple" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />
