396fc0065f0001df4ea2c85acd6a0a6a00a221b7
Alan <alanrsoars@gmail.com>
2022-05-17 10:38:02 +1200

    chore: fix trpc setup

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,27 +1,13 @@
 import { inferAsyncReturnType } from "@trpc/server";
+import { CreateNextContextOptions } from "@trpc/server/adapters/next";
 import got from "got";
-import { IronSession } from "iron-session";
-import withSession from "lib/session";
-import { NextApiRequest, NextApiResponse } from "next";
 
-type NextRequestWithSession = NextApiRequest & { session: IronSession };
-
-const createContextInner = async (
-  req: NextRequestWithSession,
-  res: NextApiResponse
-) => {
+export async function createContext(opts?: CreateNextContextOptions) {
   return {
-    req,
-    res,
+    req: opts?.req,
+    res: opts?.res,
     got,
-    session: req.session,
   };
-};
+}
 
-/**
- * Creates context for an incoming request
- * @link https://trpc.io/docs/context
- */
-export const createContext = withSession(createContextInner);
-
-export type Context = inferAsyncReturnType<typeof createContextInner>;
+export type Context = inferAsyncReturnType<typeof createContext>;
